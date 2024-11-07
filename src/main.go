d598c137a9b3a4bf79b314d3fbe85e6bba8e3256
RiyanNaffa <riyannaffanusafara@gmail.com>
2024-11-08 05:42:21 +0700

    fix: change endpoint pattern

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"book-store/api"
+
+	"github.com/savioxavier/termlink"
 )
 
 func main() {
@@ -19,12 +21,15 @@ func main() {
 	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Testing..."))
 	})
+
 	// Handle book
 	h.HandleFunc("/api/book", api.BookHandler)
 	// Handle employee
-	// h.HandleFunc("/api/employee",)
+	h.HandleFunc("/api/employee", api.EmployeeHandler)
 
-	fmt.Println("Running on Port:8080")
+	fmt.Println("Running on Port 8080")
+	fmt.Println(termlink.Link("http://localhost:8080/", "http://localhost:8080/"))
+
 	err := s.ListenAndServe()
 	if err != nil {
 		fmt.Println(err.Error())
