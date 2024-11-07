520314913fddda0925ce9717f3f01b9a06af667b
RiyanNaffa <riyannaffanusafara@gmail.com>
2024-11-08 05:41:36 +0700

    feat: switch possible methods; switch GET

--- api/book.go
+++ api/book.go
@@ -1,18 +1,60 @@
 package api
 
 import (
+	"book-store/src/service"
+	"encoding/json"
 	"net/http"
-	// "book-store/src/service"
 )
 
 func BookHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case "GET":
-		w.Write([]byte("GET api/book"))
-	case "POST":
+	case http.MethodGet:
+		action := r.URL.Query().Get("action")
+
+		switch action {
+		case "DisplayAll":
+			data, err := service.BookDisplayAll()
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(data)
+			return
+
+		case "Details":
+			id := r.URL.Query().Get("id")
+			if id == "" {
+				http.Error(w, "No ID parsed.", http.StatusBadRequest)
+				return
+			}
+			data, err := service.BookDetails(&id)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(data)
+			return
+		}
+	case http.MethodPut:
+		// Change price & stock
+		return
+	case http.MethodPost:
+		// Add a book
 		w.Write([]byte("POST api/book"))
+		return
+	case http.MethodDelete:
+		// Delete a book from database
+		return
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("405: Method Not Allowed"))
+		return
 	}
 
 }
