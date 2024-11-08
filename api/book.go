f247a050010603e3075c70a56f5576c63ff40366
RiyanNaffa <riyannaffanusafara@gmail.com>
2024-11-08 10:48:47 +0700

    fix: GET handler

--- api/book.go
+++ api/book.go
@@ -8,11 +8,13 @@ import (
 
 func BookHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+
 	case http.MethodGet:
 		action := r.URL.Query().Get("action")
 
 		switch action {
-		case "DisplayAll":
+
+		case "displayAll":
 			data, err := service.BookDisplayAll()
 
 			if err != nil {
@@ -21,11 +23,12 @@ func BookHandler(w http.ResponseWriter, r *http.Request) {
 			}
 
 			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(data)
+			json.NewEncoder(w).Encode(&data)
 			return
 
-		case "Details":
+		case "details":
 			id := r.URL.Query().Get("id")
+
 			if id == "" {
 				http.Error(w, "No ID parsed.", http.StatusBadRequest)
 				return
@@ -41,16 +44,20 @@ func BookHandler(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(data)
 			return
 		}
+
 	case http.MethodPut:
 		// Change price & stock
 		return
+
 	case http.MethodPost:
 		// Add a book
 		w.Write([]byte("POST api/book"))
 		return
+
 	case http.MethodDelete:
 		// Delete a book from database
 		return
+
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("405: Method Not Allowed"))
